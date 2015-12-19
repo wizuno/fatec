553ebef78ede73d586d30d34318015c3a869d405
edson <engsoftware1@gmail.com>
2015-12-19 21:29:17 -0200

    inclusao do log4g na classe fabrica de conexoes

--- sceweb-v1/src/br/sceweb/model/FabricaDeConexoes.java
+++ sceweb-v1/src/br/sceweb/model/FabricaDeConexoes.java
@@ -4,12 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
+
 public class FabricaDeConexoes {
+	Logger logger = Logger.getLogger(FabricaDeConexoes.class);
 	public Connection getConnection() {
 		String url = "jdbc:mysql://localhost/sceweb";
 		try {
 			return DriverManager.getConnection(url, "root", "");
 		} catch (SQLException e) {
+			logger.info("SQLException na classe FabricaDeConexoes");
 			throw new RuntimeException(e);
 		}
 	}
--- sceweb-v1/src/br/sceweb/view/FormManterEmpresa.java
+++ sceweb-v1/src/br/sceweb/view/FormManterEmpresa.java
@@ -8,9 +8,14 @@ public class FormManterEmpresa {
 		// TODO Auto-generated method stub
 		Controle controle = new Controle();
 		String msg="";
-		msg=controle.cadastrarEmpresa("89424232000180", "empresa x", "empresa x", "av. brasil", "121212");
+		try{
+		msg = controle.cadastrarEmpresa("89424232000180", "empresa x", "empresa x", "av. brasil", "121212");
+		msg = controle.excluirEmpresa("89424232000180");
+		}catch(Exception e){
+			msg = "erros no acesso a funcao cadastrarEmpresa()" + e.getMessage();
+		}
 		System.out.println(msg); 
-		System.out.println(controle.excluirEmpresa("89424232000180"));
+		
 
 	}
 
